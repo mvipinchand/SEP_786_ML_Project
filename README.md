e54c54676bac07ed9ac56c195ef1c9c2ff3f7354
Vipin Chandran <mvipinchand@gmail.com>
2023-10-19 23:22:32 -0400

    Flask app.py and Requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SEP_786_ML_Project
 This is ML project for AL &amp; ML Fundamentals Course in McMaster University
+
+conda create -p venv python==3.7 -y
+
+conda activate venv/
+
+pip install -r requiremets.txt
---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "Starting Hello World ML Project Vipin"
+
+if __name__=="__main__":
+    app.run(debug=True)
